2737f2939f17f16e0536a192355e2328e090f138
monapavasiya <mona.pavasiya08@gmail.com>
2022-08-08 12:34:04 -0400

    A

--- JavaPractice/src/fAQPrograms/OccurenceOfWordsString.java
+++ JavaPractice/src/fAQPrograms/OccurenceOfWordsString.java
@@ -5,32 +5,24 @@ import java.util.Map;
 
 public class OccurenceOfWordsString {
 
-	public static void main(String[] args) 
-	{
-		String str = "welcome to code decode and code decode welcome you";
+	public static void main(String[] args) {
+		String str = "Hi my name is mona. I am doing QA class. I will give my 100%."
+				+ " If i will give 100% then and then i will get a job in automation testing "
+				+ "field. I would love to work in this field. ";
 		
 		Map<String, Integer> map = new HashMap<String, Integer>();
 		Integer count = 1;
-		
 		String[] arr = str.split(" ");
-		
-		for(int i=0; i<arr.length; i++)
-		{
-			if(! map.containsKey(arr[i]))
-			{
+
+		for (int i = 0; i < arr.length; i++) {
+			if (!map.containsKey(arr[i])) {
 				map.put(arr[i], count);
-			}
-			else
-			{
-				map.put(arr[i], map.get(arr[i])+1);
+			} else {
+				map.put(arr[i], map.get(arr[i]) + 1);
 			}
 		}
-		
-		for(String x : map.keySet())
-		{
-			System.out.println("The count of word : "  + x + "=" + map.get(x));
+		for (String x : map.keySet()) {
+			System.out.println("The count of word : " + x + "=" + map.get(x));
 		}
-
 	}
-
 }
